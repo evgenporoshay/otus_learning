87f580c0fe41b64790cfed89c38bcdcbe2e911a1
=Evgeniy Poroschay <=89152547650@mail.ru>
2024-12-18 10:37:39 +0300

    added sql-handling.php

--- sql-handling.php
+++ sql-handling.php
@@ -1,1 +1,27 @@
 <?php
+
+use Bitrix\Main\Diag\Debug;
+use Bitrix\Main\Loader;
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+/**
+ * @var CMain $APPLICATION
+ */
+
+$APPLICATION->setTitle('Отладка SQL');
+
+Loader::includeModule('iblock');
+
+\Bitrix\Main\Application::getConnection()->setTracker();
+$queryResult = \Bitrix\Iblock\ElementTable::getList([
+    'filter' => [
+        'IBLOCK_ID' => 3,
+    ],
+    'select' => [
+        'ID'
+    ],
+]);
+\Bitrix\Main\Application::getConnection()->stopTracker();
+Debug::dump($queryResult->getTrackerQuery()->getSql());
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
