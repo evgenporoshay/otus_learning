69eb0b57b21071b8d8a363000cabdb37685f4785
Evgeniy Poroschay <89152547650@mail.ru>
2024-12-29 19:52:14 +0300

    added new files

--- logs/ex.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-try {
-    throw new Exception("Тестовое исключение");
-} catch (Exception $e) {
-    echo "Исключение поймано: " . $e->getMessage();
-}
--- logs/otus_debug.php
+++ logs/otus_debug.php
@@ -1,26 +1,21 @@
 <?php
-// Получение текущей даты и времени
+// Определяем имя файла лога
+$logFile = '../local/'. date('d-m-Y') . '.log';
+
+// Получаем текущую дату и время
 $dateTime = date('Y-m-d H:i:s');
 
-// Имя файла для записи данных
-$fileName = 'exceptions.log';
+// Формируем строку для записи в лог
+$logEntry = "OTUS - {$dateTime}\n";
 
-// Открываем файл для записи в конец
-$handle = fopen($fileName, 'a') or die("Не удалось открыть файл!");
+// Открываем файл в режиме добавления и записи
+$fileHandle = fopen($logFile, 'a') or die("Не удалось открыть файл!");
 
-// Записываем текущую дату и время в файл
-fwrite($handle, "$dateTime\n");
+// Записываем строку в файл
+fwrite($fileHandle, $logEntry);
 
-$lines = explode("\n", $message);
-
-foreach ($lines as &$line) {
-    $line = 'OTUS - ' . $line;
-}
 // Закрываем файл
-fclose($handle);
+fclose($fileHandle);
 
-// Возвращаем ответ клиенту
-header('Content-Type: text/plain');
-
-
-echo "Текущая дата и время успешно записаны в файл $fileName.";
+echo "Запись в лог выполнена.";
+?>
