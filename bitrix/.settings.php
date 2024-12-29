9341aa562c36567e17880df02e7ac7cafb0b5e07
Evgeniy Poroschay <89152547650@mail.ru>
2024-12-29 18:25:17 +0300

    Описание изменений в settings.php

--- bitrix/.settings.php
+++ bitrix/.settings.php
@@ -1,62 +1,62 @@
 <?php
 return array (
-  'cache_flags' => 
-  array (
-    'value' => 
-    array (
-      'config_options' => 3600.0,
-    ),
-    'readonly' => false,
-  ),
-  'cookies' => 
-  array (
-    'value' => 
-    array (
-      'secure' => false,
-      'http_only' => true,
-    ),
-    'readonly' => false,
-  ),
-  'exception_handling' => 
-  array (
-    'value' => 
-    array (
-      'debug' => true,
-      'handled_errors_types' => 4437,
-      'exception_errors_types' => 4437,
-      'ignore_silence' => false,
-      'assertion_throws_exception' => true,
-      'assertion_error_type' => 256,
-      'log' => 
-      array (
-        'file' => NULL,
-        'log_size' => 1000000,
-      ),
-    ),
-    'readonly' => false,
-  ),
-  'connections' => 
-  array (
-    'value' => 
-    array (
-      'default' => 
-      array (
-        'host' => 'localhost',
-        'database' => 'cj66659_bitrix',
-        'login' => 'cj66659_bitrix',
-        'password' => 'Tuz72$tvola!',
-        'options' => 2.0,
-        'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
-      ),
-    ),
-    'readonly' => true,
-  ),
-  'crypto' => 
-  array (
-    'value' => 
-    array (
-      'crypto_key' => '9fc94a6806d2b4678839e10552375d5b',
-    ),
-    'readonly' => true,
-  ),
+    'cache_flags' =>
+        array (
+            'value' =>
+                array (
+                    'config_options' => 3600.0,
+                ),
+            'readonly' => false,
+        ),
+    'cookies' =>
+        array (
+            'value' =>
+                array (
+                    'secure' => false,
+                    'http_only' => true,
+                ),
+            'readonly' => false,
+        ),
+    'exception_handling' =>
+        array (
+            'value' =>
+                array (
+                    'debug' => true,
+                    'handled_errors_types' => E_ALL & E_NOTICE & E_WARNING & E_DEPRECATED,
+                    'exception_errors_types' => 4437,
+                    'ignore_silence' => false,
+                    'assertion_throws_exception' => true,
+                    'assertion_error_type' => 256,
+                    'log' =>
+                        array (
+                            'file' => 'logs/exceptions.log',
+                            'log_size' => 1000000,
+                        ),
+                ),
+            'readonly' => false,
+        ),
+    'connections' =>
+        array (
+            'value' =>
+                array (
+                    'default' =>
+                        array (
+                            'host' => 'localhost',
+                            'database' => 'cj66659_bitrix',
+                            'login' => 'cj66659_bitrix',
+                            'password' => 'Tuz72$tvola!',
+                            'options' => 2.0,
+                            'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+                        ),
+                ),
+            'readonly' => true,
+        ),
+    'crypto' =>
+        array (
+            'value' =>
+                array (
+                    'crypto_key' => '9fc94a6806d2b4678839e10552375d5b',
+                ),
+            'readonly' => true,
+        ),
 );
