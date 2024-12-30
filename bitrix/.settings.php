b36946e6526b233209c012226f253b8afe359e3d
=Evgeniy Poroschay <=89152547650@mail.ru>
2024-12-30 10:35:08 +0300

    feat(fix): Ограничение вывода ошибок в файл лога

--- bitrix/.settings.php
+++ bitrix/.settings.php
@@ -20,8 +20,8 @@ return array(
     'exception_handling' => array (
         'value' => array (
             'debug' => true,
-            'handled_errors_types' => E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE,
-            'exception_errors_types' => E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_USER_WARNING & ~E_USER_NOTICE & ~E_COMPILE_WARNING & ~E_DEPRECATED,
+            'handled_errors_types' => E_ALL & ~E_NOTICE /*& ~E_STRICT & ~E_USER_NOTICE*/,
+            'exception_errors_types' => E_ALL & ~E_NOTICE /* & ~E_WARNING & ~E_STRICT & ~E_USER_WARNING & ~E_USER_NOTICE & ~E_COMPILE_WARNING & ~E_DEPRECATED*/,
             'ignore_silence' => false,
             'assertion_throws_exception' => true,
             'assertion_error_type' => 256,
