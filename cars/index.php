5dbcb8de411dc1ea48a756e4cc3b3d24b2af9bac
=Evgeniy Poroschay <=89152547650@mail.ru>
2025-01-09 20:34:35 +0300

    feat(add): added some code into index.php

--- cars/index.php
+++ cars/index.php
@@ -5,13 +5,6 @@ $APPLICATION ->SetTitle('Вывод связанных полей');
 use Bitrix\Main\Loader;
 use Bitrix\Iblock\Iblock;
 
-CIBlockResult
-CIBlockElement::GetList(
-    array arOrder = Array("SORT"=>"ASC"),
-        array arFilter = Array(),
-            mixed arGroupBy = false,
-	mixed arNavStartParams = false,
-	array arSelectFields = Array()
-    );
+
 
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
