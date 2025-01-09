9f74ba9164d77c06d513c072c619934c8d5dd9d9
=Evgeniy Poroschay <=89152547650@mail.ru>
2025-01-09 20:42:14 +0300

    feat(add): added some code into index.php

--- cars/index.php
+++ cars/index.php
@@ -5,6 +5,25 @@ $APPLICATION ->SetTitle('Вывод связанных полей');
 use Bitrix\Main\Loader;
 use Bitrix\Iblock\Iblock;
 
+$iblockId = 16;
+$iblockElementId = 36;
+
+// Old API
+$arFilter = ['IBLOCK_ID' => $iblockId, 'ACTIVE' => 'Y'];
+$arSelect = ['ID', 'NAME', 'CODE', 'PROPERTY_MODEL'];
+$res = CIBlockElement::GetList([], $arFilter, false, [], $arSelect);
+while($arFields = $res->fetch()){
+    pr($arFields);
+}
+
+
+$arFilter = ['IBLOCK_ID' => $iblockId];
+$arSelect = ['NAME'];
+$rsSect = CIBlockSection::GetList(['left_margin' => 'asc'], $arFilter, false, $arSelect, false);
+while ($arSect = $rsSect->fetch())
+{
+    pr($arSect);
+}
 
 
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
