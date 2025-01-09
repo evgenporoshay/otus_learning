9e5e4ee9f56c4b99e4926d7a4c06fa4c2c8d0451
=Evgeniy Poroschay <=89152547650@mail.ru>
2025-01-09 19:55:37 +0300

    feat(add): added some code into index.php

--- cars/index.php
+++ cars/index.php
@@ -1,5 +1,10 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+$APPLICATION ->SetTitle('Вывод связанных полей');
+
+use Bitrix\Main\Loader;
+use Bitrix\Iblock\Iblock;
+
 
 
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
